f2a0d1b4c8193cdf2ed30006373e4a551e3c8b5f
meaghanbass <meaghan.bass@gmail.com>
2020-03-02 13:39:20 -0800

    have to stop to work on another project, left off at Related Blogs frontend video

--- frontend/config.js
+++ frontend/config.js
@@ -1,20 +1,12 @@
 import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
-const {publicRuntimeConfig} = getConfig();
+export const API = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.API_PRODUCTION
+    : publicRuntimeConfig.API_DEVELOPMENT;
 
-export const API = publicRuntimeConfig.PRODUCTION 
-    ? 'https://seoblog.com' 
-    : 'http://localhost:8000/api';
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
-// ============================== ABOVE IS ORIGINAL WORKING CONTENT
-
-// export const API = publicRuntimeConfig.PRODUCTION 
-//     ? publicRuntimeConfig.API_PRODUCTION 
-//     : publicRuntimeConfig.DEVELOPMENT;
-
-// export const APP_NAME = publicRuntimeConfig.APP_NAME;
-
-// export const DOMAIN = publicRuntimeConfig.PRODUCTION 
-// ? publicRuntimeConfig.DOMAIN_DEVELOPMENT 
-// : publicRuntimeConfig.DOMAIN_PRODUCTION;
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.DOMAIN_PRODUCTION
+    : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,20 +1,55 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { withRouter } from "next/router";
 import Layout from '../../components/Layout';
 import { useState } from 'react';
 import { listBlogsWithCategoriesAndTags } from '../../actions/blog';
-// import { API, DOMAIN, APP_NAME } from "../../config";
-
+import { API, DOMAIN, APP_NAME } from "../../config";
 import PreviewCardLg from "../../components/Blog/PreviewCardLg";
 
-const Blogs = ({ blogs, categories, tags, size }) => {
+const Blogs = ({ blogs, categories, tags, totalBlogs, blogsLimit, blogSkip, router }) => {
+    const head = () => (
+        <Head>
+            <title>Programming Blogs | {APP_NAME}</title>
+            <meta name="description" content="Programming blogs and tutorials." />
+            <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
+            <meta property="og:title" content={`Latest web development tutorials | ${APP_NAME}`} />
+            <meta property="og:description" content="Programming blogs and tutorials." />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+            <meta property="og:site_name" content={`${APP_NAME}`} />
 
-    // const head = () => (
-    //     <Head>
-    //         <title>Test this title | {APP_NAME}</title>
-    //     </Head>
-    // )
+            <meta property="og:image" content="/images/seoblog.png" />
+            <meta property="og:image:secure_url" content="/images/seoblog.png" />
+            <meta property="og:image:type" content="image/png" />
+            {/* <meta property="og:app_id" content={`${APP_NAME}`} /> */}
+        </Head>
+    );
 
+    const [limit, setLimit] = useState(blogsLimit);
+    const [skip, setSkip] = useState(0);
+    const [size, setSize] = useState(totalBlogs);
+    const [loadedBlogs, setLoadedBlogs] = useState([]);
+
+    const loadMore = () => {
+        let toSkip = skip + limit;
+        listBlogsWithCategoriesAndTags(toSkip, limit).then(data => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                setLoadedBlogs([...loadedBlogs, ...data.blogs]);
+                setSize(data.size);
+                setSkip(toSkip);
+            }
+        })
+    };
+
+    const loadMoreButton = () => {
+        return (
+            size > 0 && size >= limit && (<button onClick={loadMore} className="btn btn-outline-primary btn-lg">Load More</button>)
+        )
+    };
+    
     const showAllBlogs = () => {
         return blogs.map((blog, i) => {
             return (
@@ -41,32 +76,49 @@ const Blogs = ({ blogs, categories, tags, size }) => {
         ))
     };
 
+    const showLoadedBlogs = () => {
+        return loadedBlogs.map((blog, i) => (
+            <article key={i}>
+                <PreviewCardLg blog={blog} />
+            </article>
+        ))
+    }
+
     return (
-        <Layout>
-            <main>
-                <div className="container-fluid">
-                    <header>
-                        <div className="col-md-12 pt-3">
-                            <h1 className="display-4 font-weight-bold text-center">Blogs</h1>
-                        </div>
-                        <section>
-                            {showAllCategories()}
-                            {showAllTags()}
-                        </section>
-                    </header>
-                </div>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-12">{showAllBlogs()}</div>
+        <>
+            {head()}
+            <Layout>
+                <main>
+                    <div className="container-fluid">
+                        <header>
+                            <div className="col-md-12 pt-3">
+                                <h1 className="display-4 font-weight-bold text-center">Blogs</h1>
+                            </div>
+                            <section>
+                                {showAllCategories()}
+                                {showAllTags()}
+                            </section>
+                        </header>
                     </div>
-                </div>
-            </main>
-        </Layout>
+                    <div className="container-fluid">
+                        {showAllBlogs()}
+                    </div>
+                    <div className="container-fluid">
+                        {showLoadedBlogs()}
+                    </div>
+                    <div className="text-center pt-5 pb-5">
+                        {loadMoreButton()}
+                    </div>
+                </main>
+            </Layout>
+        </>
     );
 };
 
 Blogs.getInitialProps = () => {
-    return listBlogsWithCategoriesAndTags().then(data => {
+    let skip = 0;
+    let limit = 2;
+    return listBlogsWithCategoriesAndTags(skip, limit).then(data => {
         if (data.error) {
             console.log(data.error);
         } else {
@@ -74,10 +126,12 @@ Blogs.getInitialProps = () => {
                 blogs: data.blogs,
                 categories: data.categories,
                 tags: data.tags,
-                size: data.size
+                totalBlogs: data.size,
+                blogsLimit: limit,
+                blogSkip: skip
             };
         }
     });
 };
 
-export default Blogs;
+export default withRouter(Blogs);
